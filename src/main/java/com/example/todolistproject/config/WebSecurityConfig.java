3616cce71a1adbb85fda552cccd9cc3d9d8af532
elina2323 <83224179+elina2323@users.noreply.github.com>
2021-12-27 14:15:56 +0600

    Update WebSecurityConfig.java

--- src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
@@ -44,6 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements A
         http
                 .httpBasic().disable()
                 .csrf().disable()
+                .cors().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
