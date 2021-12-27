35ff0ce81ddac781ce9682f6caf0516403c6c39f
elina2323 <83224179+elina2323@users.noreply.github.com>
2021-12-27 14:46:34 +0600

    Update WebSecurityConfig.java

--- src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
@@ -13,7 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
 
@@ -56,5 +62,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements A
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
     }
+     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
 
 }
