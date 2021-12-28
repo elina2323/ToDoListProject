f642c89183a9534c7956fb5c1ae49ffdcf3a45d9
elina2323 <83224179+elina2323@users.noreply.github.com>
2021-12-28 10:13:28 +0600

    Update WebSecurityConfig.java

--- src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 
 @EnableWebSecurity
