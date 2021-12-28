57dbe90b256128cd2c28115b4b3afe019e3e8826
qwerty123 <elina.balabaeva23@gmail.com>
2021-12-28 10:19:25 +0600

    Commit config, Controller, pom

--- src/main/java/com/example/todolistproject/config/SpringFoxConfig.java
+++ src/main/java/com/example/todolistproject/config/SpringFoxConfig.java
@@ -3,7 +3,6 @@ package com.example.todolistproject.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -44,6 +43,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     private ApiKey apiKey() {
         return new ApiKey("jwtToken", "Authorization", "header");
     }
+
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("swagger-ui.html")
