9954992113ff0d465b182b5d7bea9257f442d089
qwerty123 <elina.balabaeva23@gmail.com>
2021-12-28 13:30:15 +0600

    Commit config

--- src/main/java/com/example/todolistproject/config/SpringFoxConfig.java
+++ src/main/java/com/example/todolistproject/config/SpringFoxConfig.java
@@ -43,11 +43,11 @@ public class SpringFoxConfig implements WebMvcConfigurer {
          return new ApiKey("jwtToken", "Authorization", "header");
      }
 
-//   @Override
-//   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//       registry.addResourceHandler("swagger-ui.html")
-//               .addResourceLocations("classpath:/META-INF/resources/");
-//       registry.addResourceHandler("/webjars/**")
-//               .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//   }
+   @Override
+   public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       registry.addResourceHandler("swagger-ui.html")
+               .addResourceLocations("classpath:/META-INF/resources/");
+       registry.addResourceHandler("/webjars/**")
+               .addResourceLocations("classpath:/META-INF/resources/webjars/");
+   }
 }
