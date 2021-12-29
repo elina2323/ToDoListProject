0ea2b3c54d55511d468a939494947b7302bb23e0
qwerty123 <elina.balabaeva23@gmail.com>
2021-12-29 08:56:44 +0600

    Commit config

--- src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/todolistproject/config/WebSecurityConfig.java
@@ -111,8 +111,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements A
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .apply(new JwtConfigurer(jwtTokenProvider));
-                //.formLogin();
+                //.apply(new JwtConfigurer(jwtTokenProvider));
+                .formLogin();
     }
 
 }
